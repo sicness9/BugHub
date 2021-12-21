3639159bbf112ecaa8b815b239a752eb61bf0ec3
sicness9 <30636804+sicness9@users.noreply.github.com>
2021-12-20 22:05:23 -0600

    call new template if user is admin or not

--- app/teams/views.py
+++ app/teams/views.py
@@ -1,6 +1,6 @@
 # teams and team management views
 
-from flask import render_template, request, url_for, session, redirect, flash
+from flask import render_template, session, url_for, request, redirect
 from flask_cors import cross_origin
 
 from app.utils import requires_auth
@@ -15,36 +15,67 @@ from .import teams
 @requires_auth
 def team_home_page():
     current_user = session['profile']
-    all_teams = Team.query.all()
 
-    '''Check number of members for developer team'''
+    user = User.query.filter_by(email=current_user['name']).first()
+
+    if user.is_admin is not True:
+        return render_template('teams/no_team_invite.html', userinfo=current_user)
+
+    '''
+    # Check number of members for developer team
     # query for dev team in table
     dev_team = Team.query.filter_by(team_name='Developer').first()
     # check number of members
     dev_members = 0 + len(User.query.filter_by(team_id=dev_team.id).all())
 
-    '''Check for number of QA members'''
+    # Check for number of QA members
     # query for qa team in table
     qa_team = Team.query.filter_by(team_name='Quality Assurance').first()
     # check number of members
     qa_members = 0 + len(User.query.filter_by(team_id=qa_team.id).all())
 
-    '''Check for number of Support members'''
+    # Check for number of Support members
     # query for support team in table
     support_team = Team.query.filter_by(team_name='Support').first()
     # check number of members
     support_members = 0 + len(User.query.filter_by(team_id=support_team.id).all())
 
-    '''Check for number of Engineer members'''
+    # Check for number of Engineer members
     # query for support team in table
     eng_team = Team.query.filter_by(team_name='Engineer').first()
     # check number of members
-    eng_members = 0 + len(User.query.filter_by(team_id=eng_team.id).all())
+    eng_members = 0 + len(User.query.filter_by(team_id=eng_team.id).all())'''
 
-    return render_template('teams/team_home_page.html',
-                           userinfo=current_user, all_teams=all_teams,
-                           dev_members=dev_members, qa_members=qa_members, support_members=support_members,
-                           eng_members=eng_members)
+    return redirect(url_for('teams.create_team'))
+
+
+# create a team
+@teams.route("/team_create", methods=['GET', 'POST'])
+@cross_origin(headers=["Content Type", "Authorization"])
+@requires_auth
+def create_team():
+    current_user = session['profile']
+
+    user = User.query.filter_by(email=current_user['name']).first()
+
+    # create new team and send to team dashboard
+    if request.method == 'POST':
+        team_name = request.form.get('team_name')
+        admin_id = user.id
+
+        new_team = Team(team_name=team_name, admin_id=admin_id)
+        db.session.add(new_team)
+        db.session.commit()
+
+        new_team = Team.query.filter_by(team_name=new_team.team_name).first()
+
+        # update the admin to be assigned to team id
+        user.team_id = new_team.id
+        db.session.commit()
+
+        return redirect(url_for('dashboard.dashboard'))
+
+    return render_template('teams/create_team.html', userinfo=current_user)
 
 
 # developer team page

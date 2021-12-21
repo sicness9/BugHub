4b8377e2b5ed2b6bc09b55a10686cb613e2bd013
sicness9 <30636804+sicness9@users.noreply.github.com>
2021-12-20 21:23:34 -0600

    update variables to properly use .env secrets

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,31 @@
 import os
 
+from dotenv import load_dotenv
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-BH_DB_USER = os.getenv('BH_DB_USER')
-BH_DB_PASSWORD = os.getenv('BH_DB_PASSWORD')
-BH_DB_HOST = os.getenv('BH_DB_HOST')
-BH_DB = os.getenv('BH_DB')
 
-DB_URL = f'postgresql://{BH_DB_USER}:{BH_DB_PASSWORD}@{BH_DB_HOST}/{BH_DB}'
+load_dotenv()
+
+db = SQLAlchemy()
+
+db_user = os.getenv('BH_DB_USER')
+db_password = os.getenv('BH_DB_PASSWORD')
+db_host = os.getenv('BH_DB_HOST')
+database = os.getenv('BH_DB')
+
+DB_URL = f'postgresql://{db_user}:{db_password}@{db_host}/{database}'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hbxirqsh:PDWwLidXZb8oo5xfDbvY5_FzHlo0OhdA@castor.db' \
-                                            '.elephantsql.com/hbxirqsh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    from flask_migrate import Migrate
+    migrate = Migrate(app, db)
 
     from .auth import auth
     from .main_page import main_page
@@ -34,7 +41,7 @@ def create_app():
     app.register_blueprint(teams, url_prefix='/teams')
     app.register_blueprint(bugs_board, url_prefix='/bugs')
 
-    from app.database.models import User, Ticket, Team
+    from app.database.models import User, Ticket, Team, Role
 
     create_database(app)
 
@@ -43,3 +50,5 @@ def create_app():
 
 def create_database(app):
     db.create_all(app=app)
+
+

b84a15defb2ee55cd24f4f37096a026c7a482b6f
sicness9 <30636804+sicness9@users.noreply.github.com>
2021-12-20 22:05:06 -0600

    add initial roles in DB if they are not there. These are default roles

--- app/main_page/views.py
+++ app/main_page/views.py
@@ -5,12 +5,14 @@ from flask_cors import cross_origin
 
 from . import main_page
 from app.utils import requires_auth
-from app.database.models import Ticket
+from app.database.models import Ticket, Role
+from app import db
 
 
 # main page but redirect to log in first
 @main_page.route('/', methods=['GET', 'POST'])
 def main_redirect():
+
     return redirect(url_for('auth.login'))
 
 
@@ -20,6 +22,14 @@ def main_redirect():
 @cross_origin(headers=["Content Type", "Authorization"])
 @requires_auth
 def main():
-
     all_tickets = Ticket.query.all()
+
+    # initial db set up for default roles
+    db.session.add(Role(role_name='Developer'))
+    db.session.add(Role(role_name='Quality Assurance'))
+    db.session.add(Role(role_name='Support'))
+    db.session.add(Role(role_name='Engineer'))
+    db.session.add(Role(role_name='Admin'))
+    db.session.commit()
+
     return render_template('main_page/main_page.html', userinfo=session['profile'], all_tickets=all_tickets)

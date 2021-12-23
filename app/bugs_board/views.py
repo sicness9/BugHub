dad5b53d47a91fafd793b89acd3e40a74b646b8b
sicness9 <30636804+sicness9@users.noreply.github.com>
2021-12-23 13:53:50 -0600

    Added logic so that the bugs main page shows only tickets for the team that is currently signed in

--- app/bugs_board/views.py
+++ app/bugs_board/views.py
@@ -15,16 +15,63 @@ from . import bugs_board
 @requires_auth
 def bugs_main():
     current_user = session['profile']
+    user = User.query.filter_by(email=current_user['name']).first()
+    team = Team.query.filter_by(id=user.team_id).first()
 
-    in_progress_tickets = Ticket.query.filter_by(status='In Progress').all()
-    pending_triage_tickets = Ticket.query.filter_by(status='Pending Triage').all()
-    closed_tickets = Ticket.query.filter_by(status='Closed').all()
-    todo_tickets = Ticket.query.filter_by(status='To Do').all()
-    all_tickets = Ticket.query.all()
+    in_progress_tickets = Ticket.query.filter_by(status='In Progress', team_id=team.id).all()
+
+    '''in_progress_tickets = []
+    for ticket in in_progress:
+        owner = ticket.owner
+        if team.id != owner.team_id:
+            continue
+
+        in_progress_tickets.append(ticket)'''
+
+    pending_triage_tickets = Ticket.query.filter_by(status='Pending Triage', team_id=team.id).all()
+
+    '''pending_triage_tickets = []
+    for ticket in pending_triage:
+        owner = ticket.owner
+        if team.id != owner.team_id:
+            continue
+
+        pending_triage.append(ticket)'''
+
+    closed_tickets = Ticket.query.filter_by(status='Closed', team_id=team.id).all()
+
+    '''closed_tickets = []
+    for ticket in closed_tickets_query:
+        owner = ticket.owner
+        if team.id != owner.team_id:
+            continue
+
+        closed_tickets.append(ticket)'''
+
+    todo_tickets = Ticket.query.filter_by(status='To Do', team_id=team.id).all()
+
+    '''todo_tickets = []
+    for ticket in todo_tickets_query:
+        owner = ticket.owner
+        if team.id != owner.team_id:
+            continue
+
+        todo_tickets.append(ticket)'''
+
+    all_tickets = Ticket.query.filter_by(team_id=team.id).all()
+
+    '''all_tickets = []
+    for ticket in all_tickets_query:
+        owner = ticket.owner
+        if team.id != owner.team_id:
+            continue
+
+        all_tickets.append(ticket)'''
 
     return render_template('bugs_board/bugs_main.html', userinfo=current_user,
                            in_progress_tickets=in_progress_tickets, pending_triage_tickets=pending_triage_tickets,
-                           closed_tickets=closed_tickets, todo_tickets=todo_tickets, all_tickets=all_tickets)
+                           closed_tickets=closed_tickets, todo_tickets=todo_tickets, all_tickets=all_tickets, user=user,
+                           team=team)
 
 
 # ticket creation form
@@ -33,9 +80,7 @@ def bugs_main():
 @requires_auth
 def create_ticket():
     current_user = session['profile']
-
     user = User.query.filter_by(email=current_user['name']).first()
-    all_tickets = Ticket.query.all()
 
     if request.method == 'POST':
         title = request.form.get('title')
@@ -43,6 +88,7 @@ def create_ticket():
         bucket = request.form.get('bucket')
         status = request.form.get('status')
         owner_id = user.id
+        team_id = user.team_id
 
         if len(title) < 1:
             flash("Title can not empty", category='error')
@@ -54,14 +100,14 @@ def create_ticket():
             flash("Ticket must have a status", category='error')
         else:
             new_ticket = Ticket(title=title, ticket_description=ticket_description, bucket=bucket, status=status,
-                                owner_id=owner_id)
+                                owner_id=owner_id, team_id=team_id)
             user.tickets += 1
             db.session.add(new_ticket)
             db.session.commit()
 
             return redirect(url_for('bugs_board.bugs_main'))
 
-    return render_template('bugs_board/create_ticket.html', userinfo=current_user, all_tickets=all_tickets)
+    return render_template('bugs_board/create_ticket.html', userinfo=current_user, user=user)
 
 
 # make a view for ticket information based on ticket number
@@ -70,6 +116,9 @@ def create_ticket():
 @requires_auth
 def view_ticket(id):
     current_user = session['profile']
+    user = User.query.filter_by(email=current_user['name']).first()
+    team = Team.query.filter_by(id=user.team_id).first()
+    all_tickets = Ticket.query.all()  # query for all tickets for search bar
 
     # take the input from the search and split it to grab the ID value only
     form_search = request.form.get('id')
@@ -77,16 +126,16 @@ def view_ticket(id):
 
     id = form_split[0]
 
+    # query for ticket by the id and its owner
     ticket = Ticket.query.filter_by(id=id).first()
     ticket_owner = User.query.filter_by(id=ticket.owner_id).first()
-    active_users = User.query.all()
 
-    ticket_owners_team = Team.query.filter_by(id=ticket_owner.team_id).first()
-    all_tickets = Ticket.query.all()
+    # Query for all users on the same team to allow for ticket assignment
+    active_team_users = User.query.filter_by(team_id=team.id).all()
 
     return render_template('bugs_board/ticket.html', userinfo=current_user, ticket=ticket, id=id,
-                           ticket_owner=ticket_owner, active_users=active_users, ticket_owners_team=ticket_owners_team,
-                           all_tickets=all_tickets)
+                           ticket_owner=ticket_owner, active_team_users=active_team_users,
+                           all_tickets=all_tickets, user=user)
 
 
 # make updates to the ticket
@@ -95,6 +144,7 @@ def view_ticket(id):
 @requires_auth
 def update_ticket(id):
     current_user = session['profile']
+    user = User.query.filter_by(email=current_user['name']).first()
     ticket = Ticket.query.filter_by(id=id).first()
     ticket_owner = User.query.filter_by(id=ticket.owner_id).first()
     all_tickets = Ticket.query.all()
@@ -107,7 +157,7 @@ def update_ticket(id):
         ticket.bucket = request.form.get('bucket')
         ticket.status = request.form.get('status')
 
-        # grab new owner from datalist
+        # grab new owner from data list
         owner_select = request.form.get('owner_id')
         # print("owner select:", owner_select)
 
@@ -134,7 +184,7 @@ def update_ticket(id):
         db.session.commit()
 
     return render_template('bugs_board/ticket.html', userinfo=current_user, id=ticket.id, ticket=ticket,
-                           ticket_owner=ticket_owner, all_tickets=all_tickets)
+                           ticket_owner=ticket_owner, all_tickets=all_tickets, user=user)
 
 
 # delete a ticket
@@ -142,7 +192,6 @@ def update_ticket(id):
 @cross_origin(headers=["Content Type", "Authorization"])
 @requires_auth
 def delete_ticket(id):
-    current_user = session['profile']
     id = request.form.get('id')
     ticket = Ticket.query.filter_by(id=id).first()
 

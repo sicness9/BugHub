2c2fc6e9ce6db77b758f071bb669b9e66fa36ea9
sicness9 <30636804+sicness9@users.noreply.github.com>
2021-12-23 13:53:22 -0600

    added some logic to flip invite flags and functions for accepting invites and handling callback redirects

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,9 +24,7 @@ ACCESS_TOKEN_URL = os.getenv('ACCESS_TOKEN_URL')
 AUTHORIZE_URL = os.getenv('AUTHORIZE_URL')
 API_AUDIENCE = os.getenv('API_AUDIENCE')
 
-
 oauth = OAuth(app)
-
 
 # initialize authlib
 auth0 = oauth.register(
@@ -66,6 +64,14 @@ def callback_handling():
 
     # check if email exists in db
     user = User.query.filter_by(email=userinfo['name']).first()
+
+    # update the existing profile and flip invite status to accept
+    if user.invite_status == 1:
+        # invite status 2 means accepted
+        user.invite_status = 2
+
+        db.session.commit()
+
     # if email does not exist, add to User table
     if user is None:
         new_user = User(email=userinfo['name'], created_at=datetime.datetime.now())
@@ -88,3 +94,37 @@ def logout():
     # redirect to logout endpoint
     params = {'returnTo': url_for('main_page.main_redirect', _external=True), 'client_id': CLIENT_ID}
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+# once invite email is accepted, have user sign up or sign in via auth0
+@auth.route('/accept_invite', methods=['GET', 'POST'])
+def accept_invite():
+    return auth0.authorize_redirect(redirect_uri=url_for('auth.invite_callback_handling', _external=True),
+                                    audience=API_AUDIENCE)
+
+
+# redirect to profile creation and update their invite status
+@auth.route('/invite/callback', methods=['GET', 'POST'])
+def invite_callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # store user info in flask session
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+
+    # check if email exists in db
+    user = User.query.filter_by(email=userinfo['name']).first()
+    # update the existing profile and flip invite status to accept
+    if user.invite_status == 1:
+        # invite status 2 means accepted
+        user.invite_status = 2
+
+        db.session.commit()
+
+    return redirect(url_for('user_home.init_profile'))
